f965afada54323d91d4163418fcc0c37ece9dcff
cutsea110 <cutsea110@gmail.com>
2015-01-21 12:53:45 +0900

    [Refactor] Define combinator

--- Main.hs
+++ Main.hs
@@ -4,6 +4,7 @@ import Control.Arrow
 import Data.Aeson
 import Data.ByteString.Lazy (ByteString)
 import Data.Time
+import Data.Time.Calendar (Day, fromGregorian)
 
 import qualified Stocks as S
 import Query
@@ -13,7 +14,8 @@ import Util
 
 main :: IO ()
 main = withDB $ \conn -> do
-  xs <- collect findByCode "9475-T" conn
+  let y2014 = (fromGregorian 2014 1 1, fromGregorian 2014 12 31)
+  xs <- collect findByCodeBetween ("9475-T", y2014) conn
   print $ map (S.day &&& S.closingprice) xs
 
 --   xs <- runQuery conn (relationalQuery findByCode) "9475-T"
--- Query.hs
+++ Query.hs
@@ -1,35 +1,46 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Query where
 
+import Control.Applicative ((<$>))
 import Data.Int (Int32)
 import Data.Time.Calendar (Day)
+import Database.Record.Persistable
 import Database.Relational.Query
 
 import Stocks (Stocks, stocks)
 import qualified Stocks as S
 
+wheres'
+  :: (SqlProjectable p, PersistableWidth t) =>
+     (t1 -> p t -> a -> (b, b1)) -> t1 -> a -> (PlaceHolders t, b)
+wheres' p s = (fst <$>) . placeholder (p s)
+
+whereByCode :: MonadRestrict Flat m => Projection Flat Stocks -> m (PlaceHolders String)
+whereByCode s = (fst <$>) . placeholder $ \ph' -> wheres $ s ! S.code' .=. ph'
+
+whereBetween :: MonadRestrict Flat m => Projection Flat Stocks -> m (PlaceHolders (Day, Day))
+whereBetween s = (fst <$>) . placeholder $ \ph' -> do
+  wheres $ ph' ! fst' .<=. s ! S.day' `and'` s ! S.day' .<=. ph' ! snd'
+
 findByCodeBetween :: Relation (String, (Day, Day)) Stocks
 findByCodeBetween = relation' $ do
   s <- query stocks
-  (ph, ()) <- placeholder $ \cft ->
-    wheres $ s ! S.code' .=. cft ! fst' `and'`
-             s ! S.day' .>=. cft ! snd' ! fst' `and'`
-             s ! S.day' .<=. cft ! snd' ! snd'
+  cph <- whereByCode s
+  dph <- whereBetween s
   asc $ s ! S.day'
-  return (ph, s)
+  return (cph >< dph, s)
 
 findBetween :: Relation (Day, Day) Stocks
 findBetween = relation' $ do
   s <- query stocks
-  (ph, ()) <- placeholder $ \bw ->
-    wheres $ s ! S.day' .>=. bw ! fst' `and'` s ! S.day' .<=. bw ! snd'
+  ph <- whereBetween s
   asc $ s ! S.day'
   return (ph, s)
 
 findByCode :: Relation String Stocks
 findByCode = relation' $ do
   s <- query stocks
-  (ph, ()) <- placeholder $ \code ->
-    wheres $ s ! S.code' .=. code
+  ph <- whereByCode s
   asc $ s ! S.day'
   return (ph, s)
 
