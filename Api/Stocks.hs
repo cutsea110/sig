5d761c114ef482a9f8d9d666e65d90d464394a69
cutsea110 <cutsea110@gmail.com>
2015-01-24 23:56:51 +0900

    Define resource for Stocks

---
+++ Api/Stocks.hs
@@ -0,0 +1,25 @@
+module Api.Stocks (resource) where
+
+import Control.Monad.Reader
+
+import Rest
+import qualified Rest.Resource as R
+
+import Stocks (Stocks)
+import Query
+import Util
+
+type Code = String
+
+resource :: Resource IO (ReaderT Code IO) Code Void Void
+resource = mkResourceReader
+           { R.name = "stocks"
+           , R.schema = noListing $ named [("code", singleBy id)]
+           , R.get = Just get
+           }
+
+get :: Handler (ReaderT Code IO)
+get = mkIdHandler xmlJsonO $ \_ cd -> liftIO $ readStocks cd
+    where
+      readStocks :: Code -> IO [Stocks]
+      readStocks = withDB . collect' findByCode
--- Util.hs
+++ Util.hs
@@ -3,7 +3,7 @@ module Util where
 import Control.Applicative ((<$>))
 import Database.Relational.Query (relationalQuery)
 import Database.HDBC.Session (withConnectionIO, handleSqlError')
-import Database.HDBC.Record.Query (runQuery)
+import Database.HDBC.Record.Query (runQuery, runQuery')
 import Database.HDBC.PostgreSQL (Connection)
 import Database.Record.Persistable
 import Database.Relational.Query
@@ -14,8 +14,9 @@ withDB = handleSqlError' . withConnectionIO connect
 
 collect rel param = \conn -> runQuery conn (relationalQuery rel) param
 
---wheres' :: (SqlProjectable p, PersistableWidth t, Functor f, Monad f) =>
---     (t1 -> p t -> f b) -> t1 -> f (PlaceHolders t)
+-- | strict version of collect
+collect' rel param = \conn -> runQuery' conn (relationalQuery rel) param
+
 wheres' :: (SqlProjectable p, PersistableWidth t, Functor f, Monad f) =>
      ((p t, r) -> f b) -> r -> f (PlaceHolders t)
 wheres' p s = (fst <$>) . placeholder $ flip (curry p) s
