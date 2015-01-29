968421b32790163dad4f33cb51c73426f370ed4f
cutsea110 <cutsea110@gmail.com>
2015-01-29 12:07:01 +0900

    [Refactor] remove DB module

--- sig-api/Stock.hs
+++ sig-api/Stock.hs
@@ -1,28 +1,29 @@
 {-# LANGUAGE
     DeriveDataTypeable
   , DeriveGeneric
+  , FlexibleInstances
+  , MultiParamTypeClasses
   , TemplateHaskell
   , TypeFamilies
  #-}
-module Stock
-       ( Stock(..)
-       , module DB
-       , Stocks(..)
-       )
-       where
+module Stock where
 
 import Data.Aeson
 import Data.JSON.Schema
 import Data.Typeable
+import Database.HDBC.Query.TH (defineTableFromDB)
+import Database.HDBC.Schema.PostgreSQL (driverPostgreSQL)
+import Database.Record.TH (derivingShow, derivingEq)
 import Generics.Regular
 import Generics.Regular.XmlPickler
 import GHC.Generics
-import Generics.Regular
-import Generics.Regular.XmlPickler
 import Text.XML.HXT.Arrow.Pickle
 
-import DB
+import DataSource (connect)
 import Ext.Instances
+import Ext.TH (derivingGeneric, derivingOrd, derivingTypeable)
+
+defineTableFromDB connect driverPostgreSQL "public" "stock" [derivingEq, derivingGeneric, derivingOrd, derivingShow, derivingTypeable]
 
 deriveAll ''Stock "PFStock"
 type instance PF Stock = PFStock
