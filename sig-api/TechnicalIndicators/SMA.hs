5adf3a99c556948a6fab5707215e363ea0423d5f
cutsea110 <cutsea110@gmail.com>
2015-02-04 10:12:28 +0900

    [Refactor] like as IFPH

--- sig-api/TechnicalIndicators/SMA.hs
+++ sig-api/TechnicalIndicators/SMA.hs
@@ -1,12 +1,13 @@
 module TechnicalIndicators.SMA (sma) where
 
 import Control.Applicative ((<$>), (<*>))
-import Control.Arrow
 import Data.List (tails)
+
+cross (f, g) (x, y) = (f x, g y)
 
 -- | proposition : We suggest that the list of key value pair has been sorted by key.
 sma :: Fractional v => Int -> [(k, v)] -> [(k, v)]
-sma n = uncurry zip . (drop (n-1) *** map (average . take n) .tails) . unzip
+sma n = uncurry zip . cross (drop (n-1), map (average . take n) . tails) . unzip
 
 average :: Fractional a => [a] -> a
 average xs = s / l
