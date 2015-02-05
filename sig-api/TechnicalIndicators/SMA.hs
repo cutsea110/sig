c7215083bfb48d280791f7c5153869084195cb5a
cutsea110 <cutsea110@gmail.com>
2015-02-05 18:32:33 +0900

    [Comment] excuse for repair spec

--- sig-api/TechnicalIndicators/SMA.hs
+++ sig-api/TechnicalIndicators/SMA.hs
@@ -1,17 +1,15 @@
 module TechnicalIndicators.SMA (sma) where
 
 import Control.Applicative ((<$>), (<*>))
+import Control.Arrow ((***))
 import Data.List (tails, unfoldr)
 import Data.Maybe (isNothing, fromJust)
-
-pair (f, g) x = (f x, g x)
-cross (f, g) (x, y) = (f x, g y)
 
 -- | Global Proposition : We suggest that the list of key value pair has been sorted by key.
 --   This module just only calculate simple moving value.
 
 sma :: Fractional v => Int -> [(k, Maybe v)] -> [(k, v)]
-sma n = uncurry zip . cross (drop (n-1), map (average . take n) . tails) . unzip . cleansing
+sma n = uncurry zip . (drop (n-1) *** map (average . take n) . tails) . unzip . cleansing
 
 average :: Fractional a => [a] -> a
 average xs = s / l
@@ -19,8 +17,9 @@ average xs = s / l
       (s, l) = foldr (\a (x, y) -> (a + x, 1 + y)) (0,0) xs
 
 cleansing :: [(k, Maybe v)] -> [(k, v)]
-cleansing = uncurry zip . cross (id, repair) . unzip . dropWhile (isNothing . snd)
+cleansing = uncurry zip . (id *** repair) . unzip . dropWhile (isNothing . snd)
 
+-- | This function expect the argument list has Just value as head element.
 repair :: [Maybe a] -> [a]
 repair xxs@(Just x:xs) = unfoldr step (x, xxs)
     where
