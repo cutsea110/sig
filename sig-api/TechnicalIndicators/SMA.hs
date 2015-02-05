7261a155066c565724e3babcbe4a194928f4ed28
cutsea110 <cutsea110@gmail.com>
2015-02-05 16:18:41 +0900

    Define sanitize

--- sig-api/TechnicalIndicators/SMA.hs
+++ sig-api/TechnicalIndicators/SMA.hs
@@ -1,11 +1,13 @@
 module TechnicalIndicators.SMA (sma) where
 
 import Control.Applicative ((<$>), (<*>))
-import Data.List (tails)
+import Data.List (tails, unfoldr)
 
 cross (f, g) (x, y) = (f x, g y)
 
--- | proposition : We suggest that the list of key value pair has been sorted by key.
+-- | Global Proposition : We suggest that the list of key value pair has been sorted by key.
+--   This module just only calculate simple moving value.
+
 sma :: Fractional v => Int -> [(k, v)] -> [(k, v)]
 sma n = uncurry zip . cross (drop (n-1), map (average . take n) . tails) . unzip
 
@@ -13,3 +15,10 @@ average :: Fractional a => [a] -> a
 average xs = s / l
     where
       (s, l) = foldr (\a (x, y) -> (a + x, 1 + y)) (0,0) xs
+
+sanitize :: [Maybe a] -> [Maybe a]
+sanitize xs = unfoldr f (Nothing, xs)
+  where
+    f (old, []) = Nothing
+    f (old, (Nothing:xs)) = Just (old, (old, xs))
+    f (old, (Just x:xs)) = Just (Just x, (Just x, xs))
