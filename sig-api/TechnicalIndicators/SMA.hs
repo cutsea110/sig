e5939a3d6480599fa089b2e9e6444c30663784e0
cutsea110 <cutsea110@gmail.com>
2015-02-05 17:57:44 +0900

    cleansing and repair the argument list (key value pair list)

--- sig-api/TechnicalIndicators/SMA.hs
+++ sig-api/TechnicalIndicators/SMA.hs
@@ -2,25 +2,32 @@ module TechnicalIndicators.SMA (sma) where
 
 import Control.Applicative ((<$>), (<*>))
 import Data.List (tails, unfoldr)
+import Data.Maybe (isNothing, fromJust)
 
+pair (f, g) x = (f x, g x)
 cross (f, g) (x, y) = (f x, g y)
 
 -- | Global Proposition : We suggest that the list of key value pair has been sorted by key.
 --   This module just only calculate simple moving value.
 
-sma :: Fractional v => Int -> [(k, v)] -> [(k, v)]
-sma n = uncurry zip . cross (drop (n-1), map (average . take n) . tails) . unzip
+sma :: Fractional v => Int -> [(k, Maybe v)] -> [(k, v)]
+sma n = uncurry zip . cross (drop (n-1), map (average . take n) . tails) . unzip . cleansing
 
 average :: Fractional a => [a] -> a
 average xs = s / l
     where
       (s, l) = foldr (\a (x, y) -> (a + x, 1 + y)) (0,0) xs
 
-sanitize :: [Maybe a] -> [Maybe a]
-sanitize xs = unfoldr f (Nothing, xs)
-  where
-    f (old, []) = Nothing
-    f (old, x:xs) = Just $ maybe nothing just x
+cleansing :: [(k, Maybe v)] -> [(k, v)]
+cleansing = uncurry zip . cross (id, repair) . unzip . dropWhile (isNothing . snd)
+
+repair :: [Maybe a] -> [a]
+repair xxs@(Just x:xs) = unfoldr f (x, xxs)
+    where
+      f (old, []) = Nothing
+      f (old, x:xs) = Just $ maybe nothing just x
         where
           nothing = (old, (old, xs))
-          just x' = let new = Just x' in (new, (new, xs))
+          just new = (new, (new, xs))
+repair [] = []
+repair (Nothing:xs) = error "'repair' expect Just value as head of argument list."
