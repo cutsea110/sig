27110a6b1530aa74f67c4e516388b3f91f935355
cutsea110 <cutsea110@gmail.com>
2015-02-06 12:07:12 +0900

    cleansing before calculate simple moving average

--- sig-api/TechnicalIndicators/SMA.hs
+++ sig-api/TechnicalIndicators/SMA.hs
@@ -1,26 +1,41 @@
 module TechnicalIndicators.SMA (sma) where
 
-import Control.Applicative ((<$>), (<*>))
+import Control.Applicative ((<$>), (<*>), liftA, liftA2)
 import Data.List (tails, unfoldr)
 
+pair (f, g) x = (f x, g x)
 cross (f, g) (x, y) = (f x, g y)
 
 -- | Global Proposition : We suggest that the list of key value pair has been sorted by key.
 --   This module just only calculate simple moving value.
 
-sma :: Fractional v => Int -> [(k, v)] -> [(k, v)]
-sma n = uncurry zip . cross (drop (n-1), map (average . take n) . tails) . unzip
+sma :: Fractional v => Int -> [(k, Maybe v)] -> [(k, Maybe v)]
+sma n = uncurry zip . cross (drop (n-1), map (average . take n) . tails . cleansing) . unzip
 
 average :: Fractional a => [a] -> a
 average xs = s / l
     where
       (s, l) = foldr (\a (x, y) -> (a + x, 1 + y)) (0,0) xs
 
-sanitize :: [Maybe a] -> [Maybe a]
-sanitize xs = unfoldr f (Nothing, xs)
+cleansing :: [Maybe a] -> [Maybe a]
+cleansing xs = unfoldr f (Nothing, xs)
   where
     f (old, []) = Nothing
     f (old, x:xs) = Just $ maybe nothing just x
         where
           nothing = (old, (old, xs))
           just x' = let new = Just x' in (new, (new, xs))
+
+instance Num v => Num (Maybe v) where
+  (+) = liftA2 (+)
+  (*) = liftA2 (*)
+  (-) = liftA2 (-)
+  negate = liftA negate
+  abs = liftA abs
+  signum = liftA signum
+  fromInteger = Just . fromInteger
+
+instance Fractional v => Fractional (Maybe v) where
+  (/) = liftA2 (/)
+  recip = liftA recip
+  fromRational = Just . fromRational
