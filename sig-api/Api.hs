0ac8e71e7362d8566feddb8090f7a3d925cc9b51
cutsea110 <cutsea110@gmail.com>
2015-02-06 17:51:55 +0900

    Arrange contexts type

--- sig-api/Api.hs
+++ sig-api/Api.hs
@@ -2,15 +2,16 @@ module Api where
 
 import Rest.Api
 
+import ApiTypes
 import qualified Api.Brand as Brand
 import qualified Api.Stock as Stock
 
 -- | Define a versioned api
-api :: Api IO
+api :: Api SigApi
 api = [(mkVersion 1 0 0, Some1 sig)]
 
 -- | The entire routing table for v1.0.0 of the sig
-sig :: Router IO IO
+sig :: Router SigApi SigApi
 sig = root -/ brand
            -/ stock
     where
--- wai/Main.hs
+++ wai/Main.hs
@@ -4,9 +4,12 @@ import Network.Wai.Handler.Warp (run)
 import Rest.Driver.Wai (apiToApplication)
 
 import Api (api)
+import ApiTypes (Config(..), SigApi(..), runSigApi)
 import DataSource (connect)
 
 main :: IO ()
 main = do
   putStrLn "Starting warp server on http://localhost:3000"
-  run 3000 $ apiToApplication id api
+  con <- connect
+  let conf = Config con
+  run 3000 $ apiToApplication (runSigApi conf) api
