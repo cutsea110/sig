461422b26fa9766947b6510850ad8b5038c0e441
cutsea110 <cutsea110@gmail.com>
2015-02-23 14:45:14 +0900

    Remove label from timeline

--- sig-api/Util.hs
+++ sig-api/Util.hs
@@ -20,11 +20,3 @@ collect' rel param = \conn -> runQuery' conn (relationalQuery rel) param
 wheres' :: (SqlProjectable p, PersistableWidth t, Functor f, Monad f) =>
      ((p t, r) -> f b) -> r -> f (PlaceHolders t)
 wheres' p s = (fst <$>) . placeholder $ flip (curry p) s
-
-tuply f (a, b) = (f a, f b)
-tuply3 f (a, b, c) = (f a, f b, f c)
-tuply4 f (a, b, c, d) = (f a, f b, f c, f d)
-tuply5 f (a, b, c, d, e) = (f a, f b, f c, f d, f e)
-
-(f ~> g) x = g x . f x
-infixl 4 ~>
