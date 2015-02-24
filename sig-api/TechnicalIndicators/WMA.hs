365f0dceb0464e4e6613135ca052a5e0f1dc4167
cutsea110 <cutsea110@gmail.com>
2015-02-24 12:57:59 +0900

    Add TechnicalIndicators.WMA module

---
+++ sig-api/TechnicalIndicators/WMA.hs
@@ -0,0 +1,45 @@
+module TechnicalIndicators.WMA (wma, wma2, wma3, wma4, wma5) where
+
+-- | Simple Moving Average
+
+import Control.Applicative (Applicative, (<$>), (<*>), pure)
+
+import TechnicalIndicators.Core
+
+divBy :: Fractional a => [(a, a, t)] -> Int -> a
+divBy xs c = let (n, d, _) = xs !! c in n / d
+
+averageN :: (Applicative f, Num v, Fractional (f v)) =>
+            ((Int -> f v) -> tuples -> tuples') -> tuples -> [f v] -> tuples'
+averageN _pair ps xs = scanl plus (dup3 (pure 0)) xs `_divBy` ps
+    where
+      plus (ttl, wttl, w) x = let w' = w+1 in (ttl+x*w', wttl+w', w')
+      _divBy = _pair . divBy
+
+-- | Global Proposition : We suggest that the list of key value pair has been sorted by key.
+--   This module just only calculate simple moving value.
+
+wma :: Fractional v => Int -> [(k, Maybe v)] -> [(k, Maybe v)]
+wma = prepare ~> single averageN
+
+wma2 :: Fractional v =>
+     (Int, Int) -> [(k, Maybe v)] -> ([(k, Maybe v)], [(k, Maybe v)])
+wma2 = prepare ~> para averageN
+
+wma3 :: Fractional v =>
+     (Int, Int, Int)
+     -> [(k, Maybe v)]
+     -> ([(k, Maybe v)], [(k, Maybe v)], [(k, Maybe v)])
+wma3 = prepare ~> para3 averageN
+
+wma4 :: Fractional v =>
+     (Int, Int, Int, Int)
+     -> [(k, Maybe v)]
+     -> ([(k, Maybe v)], [(k, Maybe v)], [(k, Maybe v)], [(k, Maybe v)])
+wma4 = prepare ~> para4 averageN
+
+wma5 :: Fractional v =>
+     (Int, Int, Int, Int, Int)
+     -> [(k, Maybe v)]
+     -> ([(k, Maybe v)], [(k, Maybe v)], [(k, Maybe v)], [(k, Maybe v)], [(k, Maybe v)])
+wma5 = prepare ~> para5 averageN
